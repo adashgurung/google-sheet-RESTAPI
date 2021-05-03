75f1622635d9e25593cea8cb8bbff8bff5094ed0
Adash Gurung <adashleo@outlook.com>
2021-05-04 00:38:07 +0100

    react-hook-form-v7

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@material-ui/core";
+import { Button, Input, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useState, useRef } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -31,35 +31,37 @@ const sex = [
 ];
 
 function Form() {
-  //react-hook-form
-  const { register, handleSubmit, watch, errors } = useForm();
-
   const classes = useStyles();
 
-  /*   const nameInputRef = useRef(null);
-  const ageInputRef = useRef(null);
-  const salaryInputRef = useRef(null);
-  const hobbyInputRef = useRef(null);
-  const genderInputRef = useRef(null); */
-
-  /* const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("");
 
   const handleGender = (e) => {
     setGender(e.target.value);
-  }; */
+  };
+
+  //react-hook-form
+  const {
+    register,
+    handleSubmit,
+    watch,
+
+    formState: { errors },
+  } = useForm();
 
   //submit button
   const submitForm = (data) => {
-    // e.preventDefault();
     console.log("data");
 
-    const { name, age, salary, hobby } = data;
+    const { name, age, salary, hobby, email, gender } = data;
 
     const formData = {
-      name,
-      age,
+      //same key and value
+      name: name,
+      age: age,
       salary,
       hobby,
+      email,
+      gender,
     };
 
     axios
@@ -71,10 +73,10 @@ function Form() {
         alert("Row successfully added");
         console.log(response);
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch((error) => alert(error.message));
   };
+
+  /*   const { onChange, ...rest } = register("name", { required: true }); */
 
   return (
     <div className="form">
@@ -82,55 +84,90 @@ function Form() {
         //react-hook-form
         onSubmit={handleSubmit(submitForm)}
         className={classes.root}
-        noValidate
-        autoComplete="off"
+        //  noValidate
+        // autoComplete="off"
       >
-        {/*  TextField materialUI component uses inputRef NOT ref (useRef) */}
+        {/*  TextField materialUI component <TextField/> uses inputRef NOT ref (useRef) */}
+        <h1>React Hook Form Version-7</h1>
+
+        {!watch("name") && <h5>Please fill out the form.</h5>}
+
+        <lable htmlFor="name">Name:</lable>
         <TextField
-          name="name"
-          inputRef={register}
-          id="standard-basic"
-          label="FirstName"
-        />
-        <TextField
-          name="age"
-          inputRef={register}
-          id="filled-basic"
-          label="Age"
-          variant="filled"
-        />
-        <TextField
-          name="salary"
-          inputRef={register("exampleRequired", { required: true })}
-          id="outlined-basic"
-          label="Salary"
+          {...register("name", { required: "name is required" })}
+          id="name"
+          label="Name"
           variant="outlined"
+          //required
+          error={errors.name}
+          helperText={errors.name && "* This field is empty"}
         />
+        {errors.name && <small>{errors.name.message}</small>}
+
         <TextField
-          name="hobby"
-          inputRef={register}
-          id="outlined-basic"
-          label="Hobby"
+          {...register("age", {
+            required: "age is required",
+            maxLength: { value: 2, message: "you exeeded the maximum length" },
+            //valueAsNumber: true,
+            min: { value: 18, message: "you must be at least 18" },
+            /*    max: 99, */
+          })}
+          placeholder="Age"
+          type="number"
           variant="outlined"
+          error={errors.age}
+        />
+        {errors.age && <small>{errors.age.message}</small>}
+
+        <TextField
+          {...register("salary", {
+            required: true,
+
+            validate: (value) => typeof value === "number" || "must be number",
+
+            /*  validate: (value) => typeof value === "number" || "must be number", */
+            //valueAsNumber: true,
+          })}
+          /*  helperText={
+            (errors.salary?.type === "validate" && "*value must be a number") ||
+            (errors.salary?.type === "required" && "*salary is required")
+          } */
+          label="Salary PA"
+          variant="outlined"
+          error={errors.salary}
         />
 
-        {/*        <TextField
-          name="gender"
-          inputRef={register}
-          id="filled-select-currency"
+        {errors.salary && <small>{errors.salary.message}</small>}
+
+        <TextField
+          {...register("hobby", { required: true })}
+          label="Profession"
+          variant="outlined"
+        />
+        <TextField
+          type="email"
+          {...register("email", { required: true })}
+          label="Email Address"
+          variant="outlined"
+          //watch example
+          helperText={watch("email") && "* Email accepted"}
+        />
+        <TextField
+          {...register("gender", { required: true })}
           select
           label="Select"
           value={gender}
           onChange={handleGender}
           helperText="Please select your gender"
-          variant="filled"
+          variant="outlined"
         >
           {sex.map((option) => (
             <MenuItem key={option.value} value={option.value}>
               {option.label}
             </MenuItem>
           ))}
-        </TextField> */}
+        </TextField>
+        {/*   <input type="submit" /> */}
         <Button type="submit" variant="contained" color="primary">
           Submit
         </Button>
