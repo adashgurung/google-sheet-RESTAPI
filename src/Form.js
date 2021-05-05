39962e5cb1e76cc579dd913823f77985dc6919c8
Adash Gurung <adashleo@outlook.com>
2021-05-05 23:35:41 +0100

    fetch data from google-sheet

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import { Button, Input, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
 import { useForm } from "react-hook-form";
 import "./Form.css";
@@ -39,14 +39,38 @@ function Form() {
     setGender(e.target.value);
   };
 
+  const [tableData, setTableData] = useState([]);
+
+  const [totalSalaries, setTotalSalaries] = useState(0);
+
+  useEffect(() => {
+    const total = tableData.reduce(
+      (total, row) => (total = total + Number(row.salary)),
+      0
+    );
+    setTotalSalaries(total);
+  }, [tableData]);
+
   //react-hook-form
   const {
     register,
     handleSubmit,
     watch,
-
     formState: { errors },
   } = useForm();
+
+  const fetchData = () => {
+    axios
+      .get("https://sheet.best/api/sheets/92dee5f0-71fb-4d7b-b939-934c9f9f6352")
+      .then((response) => {
+        setTableData(response.data);
+        console.log("excel sheet data>>", response);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   //submit button
   const submitForm = (data) => {
@@ -56,11 +80,11 @@ function Form() {
 
     const formData = {
       //same key and value
-      name: name,
-      age: age,
+      name,
+      age,
       salary,
       hobby,
-      email,
+      email: email,
       gender,
     };
 
@@ -71,6 +95,7 @@ function Form() {
       )
       .then((response) => {
         alert("Row successfully added");
+        setTableData([...tableData, data]);
         console.log(response);
       })
       .catch((error) => alert(error.message));
@@ -80,6 +105,22 @@ function Form() {
 
   return (
     <div className="form">
+      <h2>Annual expenses £ {totalSalaries}</h2>
+      <table>
+        <tbody>
+          {tableData.map(({ age, hobby, name, salary, email, gender }) => (
+            <tr>
+              <td>{name}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+              <td>{hobby}</td>
+              <td>{email}</td>
+              <td>{gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <form
         //react-hook-form
         onSubmit={handleSubmit(submitForm)}
@@ -123,21 +164,25 @@ function Form() {
           {...register("salary", {
             required: true,
 
-            validate: (value) => typeof value === "number" || "must be number",
+            validate: (value) =>
+              typeof value === "number" || "salary must be in numbers",
 
-            /*  validate: (value) => typeof value === "number" || "must be number", */
-            //valueAsNumber: true,
+            valueAsNumber: true,
           })}
-          /*  helperText={
+          /* helperText={
+            errors.salary?.type === "required" && "The Salary is required"
+          } */
+
+          helperText={
             (errors.salary?.type === "validate" && "*value must be a number") ||
             (errors.salary?.type === "required" && "*salary is required")
-          } */
-          label="Salary PA"
+          }
+          label="Salary"
           variant="outlined"
           error={errors.salary}
         />
 
-        {errors.salary && <small>{errors.salary.message}</small>}
+        {errors.salary?.type && <small>{errors.salary.message}</small>}
 
         <TextField
           {...register("hobby", { required: true })}
@@ -167,10 +212,10 @@ function Form() {
             </MenuItem>
           ))}
         </TextField>
-        {/*   <input type="submit" /> */}
-        <Button type="submit" variant="contained" color="primary">
+        <input type="submit" />
+        {/* <Button type="submit" variant="contained" color="primary">
           Submit
-        </Button>
+        </Button> */}
       </form>
     </div>
   );
